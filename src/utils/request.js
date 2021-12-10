c14c5a12d8fa4957a768037890572c33573dc408
havebear <616749285@qq.com>
2021-12-10 17:35:44 +0800

    chore: init

--- src/utils/request.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: BGG
- * @Date: 2021-12-10 17:16:37
- * @LastEditors: BGG
- * @LastEditTime: 2021-12-10 17:16:37
- * @Description:  请求拦截器
- */
-
--- src/views/README.md
+++
@@ -1,7 +0,0 @@
-<!--
- * @Author: BGG
- * @Date: 2021-12-10 17:17:02
- * @LastEditors: BGG
- * @LastEditTime: 2021-12-10 17:17:02
- * @Description:  视图文件
--->
